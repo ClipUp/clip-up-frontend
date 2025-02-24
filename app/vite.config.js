fbe9df180bdd1153cef665f287ea5f4bcb99b582
sujlee <sujlee@student.42seoul.kr>
2025-02-24 21:56:32 +0900

    fix: https

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,10 +7,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    strictPort: true,
     cors: true,
     hmr: {
-      clientPort: 80,
+      clientPort: 5173,
     },
   }
 })
