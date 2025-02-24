943390d78599caf9efda0acd79a071859599c711
sujlee <sujlee412@gmail.com>
2025-02-24 22:15:45 +0900

    fix: https

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // 모든 IP에서 접근 허용
+    host: '0.0.0.0',
     port: 5173,
-    strictPort: true, // 포트가 사용 중이면 종료 (중복 방지)
-    cors: true, // CORS 활성화
-    hmr: {
-      clientPort: 80, // Nginx 프록시를 통한 HMR 연결
+    cors: {
+      origin: '*', // 모든 도메인 허용
+      methods: ['GET', 'POST'], // 허용할 HTTP 메서드
+      allowedHeaders: ['Content-Type'], // 허용할 헤더
     },
-    headers: {
-      "Access-Control-Allow-Origin": "*", // CORS 허용
+    hmr: {
+      clientPort: 5173,
     },
   },
 });
-
