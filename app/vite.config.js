180ec952f52244b0f221abe67d6a07e205f6376b
sujlee <sujlee@student.42seoul.kr>
2025-02-27 21:53:30 +0900

    fix: config

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   build: {
     outDir: 'dist', // 빌드된 파일이 저장될 디렉토리
   },
