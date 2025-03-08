1789bf60670e09710ca52aba1d8a74cc50f14215
sujlee <sujlee@student.42seoul.kr>
2025-03-09 00:24:09 +0900

    feat: 반응형 비로그인

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import './assets/fonts/pretendard.css';
 import './index.scss';
-import './App.scss';
 import './components/layout/layout.scss';
 import App from './App.jsx';
 
